9d14630d04ee3e1e04f36d46444553b537011243
SeaGithubs <seamusblally@gmail.com>
2025-09-07 22:03:42 -0600

    True Done

--- Addition.cpp
+++ Addition.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //Function to add up argv 
 int AddUp(int A[], int n) {
     int sum = 0;
-    for(int i = 0; i <= n; i++){
+    for(int i = 0; i < n; i++){
         sum = sum + A[i]; 
     }
     return sum;
@@ -14,8 +14,8 @@ int AddUp(int A[], int n) {
 
 int main(int argc, char* argv[]){
     int numbers[10];   // array for the command line argument (CLA) integers
-    if(argc <= 11){
-        for(int i = 1; i <= argc; i++){
+    if(argc < 11){
+        for(int i = 1; i < argc; i++){
             numbers[i-1] = atoi(argv[i]); //converts the CLA srings into integers and stores them
         }
         cout << AddUp(numbers, argc-1)<< endl;
