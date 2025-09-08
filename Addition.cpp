d6bdc6becd48d07c1cbc0e9c551718b68a1f6224
SeaGithubs <seamusblally@gmail.com>
2025-09-07 21:33:27 -0600

    worked?

--- Addition.cpp
+++ Addition.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 int AddUp(int A[], int n) {
-
+    int sum = 0;
+    for(int i = 0; i <= n; i++){
+        sum = sum + A[i]; 
+    }
+    return sum;
 }
 
 int main(int argc, char* argv[]){
-    int numbers[10];
+    int numbers[10] = {};
     if(argc < 11){
         for(int i = 1; i < argc; i++){
-            numbers[i-1] = int(argv[i]);
+            numbers[i-1] = atoi(argv[i]);
         }
-        AddUp(numbers, argc-1);
+        cout << AddUp(numbers, argc-1)<< endl;
     }else{
         cout << "Please use less than 10 numbers." << endl;
     }
