c1d0a0295dabe79b142d117e8c8ee4e78d0a1e15
SeaGithubs <seamusblally@gmail.com>
2025-09-30 12:51:57 -0600

    b

--- Lab_3/Submitions/DLListMain.cpp
+++ Lab_3/Submitions/DLListMain.cpp
@@ -3,10 +3,28 @@
 using namespace std;
 
 int main(){
+    DLList DLL;
     int input = -1;
     while (input != 6){
         cout << "______________________" << endl;
-        DLList traverse();
+        DLL.traverse();
+        cout << "______________________" << endl;
+        cin >> input;
+        if(input = 1){
+
+        }else if(input = 2){
+            int temp;
+            cin >> temp;
+            DLL.insertHead(temp*);
+        }else if(input = 3){
+            
+        }else if(input = 4){
+            
+        }else if(input = 5){
+            
+        }else if(input = 6){
+            
+        }
         input = 6;
     }
 }
